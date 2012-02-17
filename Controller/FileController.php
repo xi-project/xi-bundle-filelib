3cc5524d4b71befc82ace680de197e855dd79c24
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-02-17 16:57:26 +0200

    Initial stuff.

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -56,9 +56,66 @@ class FileController extends Controller
         }
 
         
+        
+        
+        
     }
     
     
+    public function renderByFilenameAction($fileName, $version = 'original', $download = false)
+    {
+        $fl = $this->get('filelib');
+        
+        $file = $fl->file()->find(36);
+        if(!$file) {
+            return $this->createNotFoundException();
+            // throw new Emerald_Common_Exception('File not found', 404);
+        }
+        $opts = array();
+
+        if ($version && $version != 'original') {
+            $opts['version'] = $version;
+        }
+        
+        if ($download) {
+            $opts['download'] = true;
+        }
+
+        die('xooxer');
+        
+        
+        
+        // When readable by anonymous, redirect to pretty url
+        if ($fl->file()->isReadableByAnonymous($file)) {
+            $url = $fl->file()->getUrl($file, $opts);
+            return $this->redirect($url, 302);
+        }
+        
+        // Convert all exceptions to 404's
+        try {
+            
+            $response = new Response();
+            
+            
+            if (isset($opts['download'])) {
+                $response->headers->set('Content-disposition', "attachment; filename={$file->getName()}");
+            }
+
+            $response->headers->set('Content-Type', $file->getMimetype());
+            
+            $response->setContent($fl->file()->render($file, $opts));
+            
+            return $response;
+            
+        } catch (\Exception $e) {
+           return $this->createNotFoundException();
+        }
+
+        
+        
+        
+        
+    }
     
     
     
