400e16007458f05cc79d22f850ee642ab73057ef
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-05-14 17:05:26 +0300

    Fabien doesn't own us!

--- Command/QueueProcessorCommand.php
+++ Command/QueueProcessorCommand.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace Xi\Bundle\FilelibBundle\Command;
 
 use Xi\Filelib\Event\FileEvent;
